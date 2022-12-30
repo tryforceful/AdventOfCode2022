c8d3241aa8eb233eada3ecc92894e27df3ec3cc2
Rob Keleher <rob.keleher@hennge.com>
2022-12-30 14:26:39 +0900

    The rest of Day 16

--- 16/Day16.ts
+++ 16/Day16.ts
@@ -2,14 +2,16 @@
  * IMPORT DATA
  */
 
-import { Queue } from 'mnemonist'
-
+import { Queue } from 'mnemonist';
 import * as fs from "fs";
 
-const PART_ONE = true;
+const PART_ONE = false;
+const SAMPLE = false;
 
-const data: string = fs.readFileSync("./day16.txt", "utf8");
-
+const data: string = fs.readFileSync(
+  SAMPLE ? "./day16_sample.txt" : "./day16.txt"
+  // './testcases/case4.txt'
+, "utf8");
 const input: string[] = data.split("\n").slice(0, -1);
 
 const rex = /Valve | has flow rate=|; tunnels? leads? to valves? /g
@@ -19,6 +21,8 @@ type ValveInfo = {
       flowrate: number;
       paths: string[];
     }
+
+// Massage Data
 
 const valvearr = input.map((line, index) => {
   const [,valve,flowrate,paths] = line.split(rex)
@@ -34,8 +38,12 @@ const valveobj = Object.fromEntries(valvearr);
 const nonzero_valves = valvearr.filter(([, info]) => info.flowrate > 0)
 const nonzero_valve_idxes = nonzero_valves.map(x => x[1].index)
 
-// console.log(valveobj);
+const START_IDX = valveobj['AA'].index || 0
 
+// console.log({ START_IDX });
+// console.log(valveobj);
+// console.log(nonzero_valves)
+// console.log(nonzero_valve_idxes)
 
 // Make Adjacency Matrix
 const matrix: number[][] = new Array(LENGTH)
@@ -61,88 +69,150 @@ function printMatrix() {
 }
 // printMatrix()
 
-// console.dir(nonzero_valve_idxes);
-
 // Now we can try to traverse the graph knowing the min paths for each i,j vertex combo
 
 const MAX_MINS = PART_ONE ? 30 : 26;
 
 const START_STATE = {
-    opened: [] as number[],
-    closed: new Set(nonzero_valve_idxes),
-    total_minutes: 0,
-    total_flow: 0,
-  }
+  opened: [] as number[],
+  minutes_so_far: 0,
+  flow_so_far: 0,
+};
+type DP_State = typeof START_STATE;
 
 const sum = (a:number,c:number) => a+c
 
 const BFS = Queue.from([START_STATE]);
 
+type T_DP_Key = string
+type T_TotalCumulativeFlow = number
+
+const DP = new Map<T_DP_Key, T_TotalCumulativeFlow>();
+DP.set("", 0); // initial state
+
+const sorter = (a:number,b:number) => a < b ? -1 : a > b ? 1 : 0
+
+function makeKeyFromArray(opened: number[]): T_DP_Key {
+  return `${[...opened].sort(sorter).join()}`;
+};
+
 let MOST_FLOW = 0;
+
+let max_size_opened = 0;
+let num_cycles = 0;
 
 function traverse() {
   while(BFS.size) {
+    num_cycles++;
+
     const node = BFS.dequeue()
-    if(!node) throw 'Uh oh'
-    const {opened, closed, total_minutes, total_flow} = node;
+    if (!node) throw "Uh oh";
+    const {opened, minutes_so_far, flow_so_far} = node;
+    const node_key = makeKeyFromArray(node.opened);
 
-    // if (
-    //   ["3", "3,1", "3,1,9", "3,1,9,7", "3,1,9,7,4", "3,1,9,7,4,2"].includes(
-    //     node.opened.join()
-    //   )
-    // )
-    //   console.dir(node);
+    if (!DP.has(node_key) === undefined) throw "More uh oh";
 
-    function runDownClock() {
-      const new_total_flow = total_flow + opened.map(
-          opened_valve_idx => valveobj[valve_names[opened_valve_idx]].flowrate * (MAX_MINS - total_minutes)
+    function runDownClock(_node: DP_State | undefined = node) {
+      if (!_node) throw "Uh oh 2";
+      const final_total_flow = _node.flow_so_far + _node.opened.map(
+          opened_valve_idx => valveobj[valve_names[opened_valve_idx]].flowrate * (MAX_MINS - _node.minutes_so_far)
         ).reduce(sum, 0)
 
-      if(new_total_flow > MOST_FLOW) {
-        MOST_FLOW = new_total_flow;
-        // console.log("Max flow was now found to be", MOST_FLOW);
+      if(final_total_flow > MOST_FLOW) {
+        MOST_FLOW = final_total_flow;
+        // console.log(
+        //   "Max flow was now found to be",
+        //   MOST_FLOW,
+        //   "with",
+        //   _node.opened.join(),
+        //   `[${_node.opened.map((idx) => valve_names[idx]).join()}]`
+        // );
       }
+
+      if(_node.opened.length > max_size_opened)
+        max_size_opened = _node.opened.length
+
+      return final_total_flow
     }
 
-    if(!closed.size) {
-      // No more to traverse! Just run down the clock and report the final flow
-      runDownClock();
+    if (opened.length === nonzero_valves.length) {
+      // No more to traverse!
       continue;
     }
 
-    for (const nextvalve of [...closed]) {
+    // determine closed set
+    const closed = nonzero_valve_idxes.filter( x => !opened.includes(x));
+
+    for (const nextvalve of closed) {
       // calculate the move to this next valve
       // according to matrix, it costs this much (+1 to open valve)
-      const dt = matrix[opened[opened.length-1] || 0][nextvalve] + 1;
+      const dt = matrix[opened[opened.length - 1] || START_IDX][nextvalve] + 1;
 
-      const new_total_minutes = total_minutes + dt;
+      const new_total_minutes = minutes_so_far + dt;
 
       if(new_total_minutes > MAX_MINS) {
         // We can't go any deeper without going over the time limit.
-        // Just calculate how much more flow we will see until the time limit
-        runDownClock();
         continue;
       }
 
       // calculate increase in total flow
-      const new_total_flow = total_flow + opened.map(
+      const new_flow_so_far = flow_so_far + opened.map(
         opened_valve_idx => valveobj[valve_names[opened_valve_idx]].flowrate * dt
       ).reduce(sum, 0)
 
-      // remove from the closed set (as a copy)
-      const new_closed_set = new Set(closed)
-      new_closed_set.delete(nextvalve);
+      const newnode: DP_State = {
+        opened: [...opened, nextvalve],
+        minutes_so_far: new_total_minutes,
+        flow_so_far: new_flow_so_far
+      }
+      const new_node_key = makeKeyFromArray(newnode.opened);
+
+      const dp_new_node = DP.get(new_node_key);
+      const new_nodes_cumulative_flow_value = runDownClock(newnode)
+      if (dp_new_node === undefined || new_nodes_cumulative_flow_value > dp_new_node) {
+        DP.set(new_node_key, new_nodes_cumulative_flow_value);
+      }
 
       // push this onto the queue
-      BFS.enqueue({
-        opened: [...opened, nextvalve],
-        closed: new_closed_set,
-        total_minutes: new_total_minutes,
-        total_flow: new_total_flow,
-      });
+      BFS.enqueue(newnode);
     }
   }
 }
 
 traverse()
-console.log("Final max flow was found to be", MOST_FLOW);
+console.log("Final max flow for 1 agent was found to be", MOST_FLOW);
+console.log(`Biggest # of opened valves in ${MAX_MINS} mins was ${max_size_opened}`);
+console.dir({num_cycles, DPsize: DP.size})
+
+// console.dir([...DP.entries()].slice(-100))
+// console.dir([...DP.values()].reduce((a,c) => Math.max(a,c)));
+// const X = [...DP.entries()].sort((a,b) => a[1] < b[1] ? 1 : a[1]>b[1] ? -1 :0)
+// console.dir(X.slice(0,100))
+
+if(!PART_ONE) {
+  console.log('Calculating max flow for 2 agents [me + elephant]...')
+  let maxvalue = 0;
+
+  DP.forEach((me_flow, me_valves_key) => {
+    const me_valves = me_valves_key.split(",").map(Number)
+
+    DP.forEach((elephant_flow, elephant_valves_key) => {
+
+      if (me_flow + elephant_flow <= MOST_FLOW) return;
+
+      const elephant_valves = elephant_valves_key.split(",").map(Number);
+
+      if(!me_valves.some(x => elephant_valves.includes(x))) {
+        // disjoint sets
+        const thismax = me_flow + elephant_flow;
+        if(thismax > maxvalue) {
+          maxvalue = thismax;
+          // console.log('updated to', maxvalue)
+          // console.dir({me_valves_key, me_flow, elephant_valves_key, elephant_flow})
+        }
+      }
+    })
+  })
+
+  console.log(maxvalue);
+}
---
+++ 16/testcases/readme.md
@@ -0,0 +1,39 @@
+# Test Cases
+
+<https://www.reddit.com/r/adventofcode/comments/znklnh/2022_day_16_some_extra_test_cases_for_day_16/>
+
+## Testcase 1 - A Line, linearly increasing rates
+
+Part 1: 2640
+2640 |AA|FA|GA|HA|IA|JA|KA|LA|MA|NA|OA|PA
+
+Part 2: 2670
+1240 |AA|DA|EA|FA|GA|HA|IA|JA|CA
+1430 |AA|KA|LA|MA|NA|OA|PA
+
+## Testcase 2 - A Line, quadratically increasing rates
+
+Part 1: 13468
+13468 |AA|IA|JA|KA|LA|MA|NA|OA|PA
+
+Part 2: 12887
+4857 |AA|FA|GA|HA|IA|JA|KA|EA|DA
+8030 |AA|LA|MA|NA|OA|PA
+
+## Testcase 3 - A circle
+
+Part 1: 1288
+1288 |AA|CA|EA|GA|IA|KA|MA|NA|OA|PA|BA
+
+Part 2: 1484
+794 |AA|CA|EA|GA|IA|HA|FA|DA
+690 |AA|OA|MA|KA|JA|LA|NA|PA|BA
+
+## Testcase 4 - Clusters
+
+Part 1: 2400
+2400 |AA|CK|CX|CZ|CY|CW
+
+Part 2: 3680
+1840 |AA|AK|AW|AX|AY|AZ
+1840 |AA|CK|CZ|CX|CY|CW
